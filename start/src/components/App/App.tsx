f5415c8b09f3ed60252727e8e220e448653b8552
akexinad <danixeka@gmail.com>
2019-12-15 15:31:22 +1100

    rendering content conditionally

--- start/src/components/App/App.tsx
+++ start/src/components/App/App.tsx
@@ -1,92 +1,109 @@
-import React, { FC, useState } from "react";
+import React, { Component } from "react";
 
 import "./App.css";
 
 import Person from "../Person/Person";
 import TodoList from "../TodoList/TodoList";
-import Button from "../Button/Button";
-
-interface Profile {
-    name: string;
-    age: number;
-}
 
 export interface TodoItem {
     id: string;
     text: string;
 }
 
-const initialProfileData = [
-    { name: "fellini", age: 44 },
-    { name: "pasolini", age: 34 },
-    { name: "benigni", age: 22 }
-];
+class App extends Component {
 
-const initialTodoData = [
-    { id: "001", text: "get milk" },
-    { id: "002", text: "eggs" }
-];
-
-const mock = [{ foo: "yes", bar: 34, baz: true }];
-
-const App: FC = () => {
-    const [profileList, setProfile] = useState<Profile[]>(initialProfileData);
-    const [todoList, setTodo] = useState<TodoItem[]>(initialTodoData);
-
-    const editProfileHandler = () => {
-        setProfile([
-            { name: "jerry", age: 99999 },
-            { name: "george", age: 34 },
-            { name: "kramer", age: 22 }
-        ]);
+    state = {
+        profileList: [
+            { name: "fellini", age: 44 },
+            { name: "pasolini", age: 34 },
+            { name: "benigni", age: 22 }
+        ],
+        todoList: [
+            { id: "001", text: "get milk" },
+            { id: "002", text: "eggs" }        
+        ],
+        showPersons: false
+    }
+    
+    editProfileHandler = () => {
+        this.setState({
+            profileList: [
+                { name: "jerry", age: 99999  },
+                { name: "george", age: 34 },
+                { name: "kramer", age: 22 }    
+            ]
+        });
     };
 
-    const addTodoHandler = (text: TodoItem["text"]) => {
-        const id: TodoItem["id"] = "00" + (todoList.length + 1);
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({
+            showPersons: !doesShow
+        })
+    }
+
+    addTodoHandler = (text: TodoItem["text"]) => {
+        const id: TodoItem["id"] = "00" + (this.state.todoList.length + 1);
 
         const newItem = {
             id,
             text
         };
 
-        setTodo(todoList => [...todoList, newItem]);
+        this.setState({
+            todoList: [
+                ...this.state.todoList,
+                newItem
+            ]
+        });
     };
 
-    console.log(todoList);
+    renderPersons = () => {
+        return (
+            this.state.showPersons ? 
+                <div>
+                    <Person
+                        click={this.editProfileHandler}
+                        user={this.state.profileList[0]}
+                        todo={this.addTodoHandler}
+                    >
+                        props.children
+                    </Person>
+                    <Person
+                        click={this.editProfileHandler}
+                        user={this.state.profileList[1]}
+                        todo={this.addTodoHandler}
+                    >
+                        props.children
+                    </Person>
+                    <Person
+                        click={this.editProfileHandler}
+                        user={this.state.profileList[2]}
+                        todo={this.addTodoHandler}
+                    >
+                        props.children
+                    </Person>
+                </div>
+            :
+            null
+        );
+    }
 
-    return (
-        <div className="App">
-            <h1>Hello World</h1>
-            <h2>Working</h2>
-            <button onClick={editProfileHandler}>Switch Name</button>
-            <Button buttonClick={mock} />
-            <Person
-                click={editProfileHandler}
-                user={profileList[0]}
-                todo={addTodoHandler}
-            >
-                props.children
-            </Person>
-            <Person
-                click={editProfileHandler}
-                user={profileList[1]}
-                todo={addTodoHandler}
-            >
-                props.children
-            </Person>
-            <Person
-                click={editProfileHandler}
-                user={profileList[2]}
-                todo={addTodoHandler}
-            >
-                props.children
-            </Person>
-            <br />
-            <br />
-            <br />
-            <TodoList todos={todoList} />
-        </div>
-    );
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World</h1>
+                <h2>Working</h2>
+                <button onClick={this.togglePersonsHandler}>Show Persons</button>
+                { this.renderPersons() }
+                <br />
+                <br />
+                <br />
+                <TodoList todos={this.state.todoList} />
+            </div>
+        );
+    }
+
 };
 
 export default App;
