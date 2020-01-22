c746e29f043aa63a0eff02faef24f032d2ee74ce
akexinad <danixeka@gmail.com>
2020-01-22 12:54:53 +1100

    assignment 2 completed

--- assignment_2/src/App.tsx
+++ assignment_2/src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { Validation } from "./ValidationComponent";
+import { Char } from "./CharComponent";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    userInput: "",
+    userInputChars: []
+  };
+
+  public handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const userInputChars = event.target.value.split("");
+
+    this.setState({ userInput: event.target.value, userInputChars });
+  };
+
+  public handleDeleteCharacter = (index: number) => {
+    const charsToBeUpdated = [...this.state.userInputChars];
+
+    charsToBeUpdated.splice(index, 1);
+
+    const inputToBeUpdated = charsToBeUpdated.join('');
+
+    this.setState({
+      userInput: inputToBeUpdated,
+      userInputChars: charsToBeUpdated
+    });
+  };
+
+  public renderInputCharacters = () => {
+    return (
+      <Fragment>
+        {this.state.userInputChars.map((character, index) => (
+          <Char
+            key={index}
+            propKey={index}
+            inputCharacter={character}
+            deleteCharacter={e => this.handleDeleteCharacter(index)}
+          />
+        ))}
+      </Fragment>
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Assignment 2</h2>
+        <input type="text" onChange={e => this.handleInputChange(e)} />
+        <h3>{this.state.userInput}</h3>
+        <Validation userInput={this.state.userInput} />
+        {this.renderInputCharacters()}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ assignment_2/src/ValidationComponent.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface InputProps {
+    userInput: string;
+}
+
+export const Validation: FC<InputProps> = ({ userInput }) => {
+    
+    return (
+        <div>
+            <h2>Validation</h2>
+            <p>{ userInput.length < 5 ? "Text too short" : "Text Long Enough" }</p>
+        </div>
+    )
+}
