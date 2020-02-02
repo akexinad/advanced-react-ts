873f3f62ea79e422a46518e582eb620b6f68a4c0
Danny <danny.ceccattini@lendlease.com>
2020-02-03 08:34:03 +1100

    174.

--- panino/src/components/Navigation/Toolbar/Toolbar.tsx
+++ panino/src/components/Navigation/Toolbar/Toolbar.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react";
 
 import styles from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
 
 const Toolbar: FC = () => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>... links go here.</nav>
     </header>
 );
