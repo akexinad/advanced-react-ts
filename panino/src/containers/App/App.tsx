ed1e137b7df3996331852fad9e72f7bfba8e1db4
akexinad <danixeka@gmail.com>
2020-03-12 10:41:00 +1100

    252. Setting Up Routing & Routes

--- panino/src/containers/App/App.tsx
+++ panino/src/containers/App/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import styles from "./App.module.css";
 import Layout from "../../hoc/Layout/Layout";
 import BurgerBuilder from "../BurgerBuilder/BurgerBuilder";
 import Checkout from "../Checkout/Checkout";
+
+import styles from "./App.module.css";
 
 const App: React.FC = () => {
     return (
         <div className={styles.App}>
             <h1>Oh Look! Panini!</h1>
             <Layout>
-                <BurgerBuilder />
-                <Checkout />
+                <Switch>
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/" exact component={BurgerBuilder} />
+                </Switch>
             </Layout>
         </div>
     );
