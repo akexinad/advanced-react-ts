11082e0c608d6b616846237e18fa64605285fc2c
akexinad <danixeka@gmail.com>
2020-03-11 15:36:27 +1100

    wip

---
+++ assignment_3/src/containers/Course/Course.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+
+class Course extends Component {
+    render () {
+        console.log('this.props', this.props)
+        return (
+            <div>
+                <h1>_COURSE_TITLE_</h1>
+                <p>You selected the Course with ID: _ID_</p>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Course);
---
+++ assignment_3/src/containers/Courses/Courses.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Course from "../Course/Course";
+
+import "./Courses.css";
+
+class Courses extends Component {
+  state = {
+    courses: [
+      { id: 1, title: "Angular - The Complete Guide" },
+      { id: 2, title: "Vue - The Complete Guide" },
+      { id: 3, title: "PWA - The Complete Guide" }
+    ]
+  };
+
+  _selectedCourse = courseId => {
+    console.log("this.props", this.props);
+    this.props.history.push({
+      pathname: "/courses/" + courseId
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Amazing Udemy Courses</h1>
+        <section className="Courses">
+          {this.state.courses.map(course => {
+            return (
+              <article
+                onClick={() => this._selectedCourse(course.id)}
+                className="Course"
+                key={course.id}
+              >
+                {course.title}
+              </article>
+            );
+          })}
+        </section>
+        <Route
+          path={this.props.match.url + "/:courseId"}
+          exact
+          component={Course}
+        />
+      </div>
+    );
+  }
+}
+
+export default Courses;
