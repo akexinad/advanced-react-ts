dc0f95ac025150bd5d5a509490f4e030b0393007
danny <danixeka@gmail.com>
2020-03-26 20:24:20 +1100

    asynchronous validations

--- panino/src/containers/App/App.tsx
+++ panino/src/containers/App/App.tsx
@@ -1,25 +1,80 @@
+import "./App.module.css";
+
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-import Layout from "../../hoc/Layout/Layout";
-import BurgerBuilder from "../BurgerBuilder/BurgerBuilder";
-import Checkout from "../Checkout/Checkout";
-
-import styles from "./App.module.css";
-import Orders from "../Orders/Orders";
+interface IReactHookForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: "Male" | "Female";
+    username: string;
+    aboutYou: string;
+}
 
 const App: React.FC = () => {
+    const { register, handleSubmit, errors } = useForm<IReactHookForm>();
+    const onSubmit = (data: any) => {
+        console.log("data", data);
+    };
+
+    const sleep = (ms: number) =>
+        new Promise(resolve => setTimeout(resolve, ms));
+
+    const validateUsername = async (value: string) => {
+        await sleep(1000);
+
+        if (value === "bill") return true;
+
+        return false;
+    };
+
     return (
-        <div className={styles.App}>
-            <h1>Oh Look! Panini!</h1>
-            <Layout>
-                <Switch>
-                    <Route path="/checkout" component={Checkout} />
-                    <Route path="/orders" exact component={Orders} />
-                    <Route path="/" exact component={BurgerBuilder} />
-                </Switch>
-            </Layout>
-        </div>
+        <form className="App" onSubmit={handleSubmit(onSubmit)}>
+            <h1>Sign Up</h1>
+            <label>First Name:</label>
+            <input
+                name="firstName"
+                ref={register({ required: true, minLength: 4 })}
+            />
+            {errors.firstName && errors.firstName.type === "required" && (
+                <p>This is required</p>
+            )}
+            {errors.firstName && errors.firstName.type === "minLength" && (
+                <p>Please use at least 4 characters</p>
+            )}
+
+            <label>Last Name:</label>
+            <input name="lastName" ref={register({ required: true })} />
+            {errors.lastName && <p>This is required</p>}
+
+            <label>Gender</label>
+            <select name="gender" ref={register({ required: true })}>
+                <option value="">Select...</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
+            {errors.gender && <p>This is required</p>}
+
+            <label>Username</label>
+            <input
+                name="username"
+                ref={register({ required: true, validate: validateUsername })}
+            />
+            {errors.username && (
+                <p>This is required</p>
+            )}
+
+            <label>Email</label>
+            <input name="email" ref={register({ required: true })} />
+            {errors.email && <p>This is required</p>}
+
+            <label>About you</label>
+            <textarea name="aboutYou" ref={register({ required: true })} />
+            {errors.aboutYou && <p>This is required</p>}
+
+            <input type="submit" />
+        </form>
     );
 };
 
